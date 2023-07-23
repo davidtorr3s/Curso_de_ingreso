201dca3440c02a39973dc2f8e287da3da894cbe4
davidtorr3s <108692798+davidtorr3s@users.noreply.github.com>
2023-07-23 19:08:25 -0300

    Update app.py

--- Curso_de_ingreso_PYTHON/ejercicios/07_tps/05_WHILE_VALIDACIONES_rising_btl/app.py
+++ Curso_de_ingreso_PYTHON/ejercicios/07_tps/05_WHILE_VALIDACIONES_rising_btl/app.py
@@ -50,7 +50,44 @@ class App(customtkinter.CTk):
         self.btn_validar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
 
     def btn_validar_on_click(self):
-        pass
+        
+        apellido = prompt("titulo", "Ingrese apellido")
+        
+        while (apellido == None or apellido == "") or not apellido.isalpha():
+            apellido = prompt("Titulo", "Reingrese apellido")
+            
+        edad = prompt(title="Edad",prompt= "Ingrese su edad")
+        while edad == None or not edad.isdigit() or int(edad) < 18 or int(edad) > 90:
+            edad = prompt(title="Edad",prompt= "Ingrese una edad valida")
+        edad = int(edad)
+        
+        estado_civil = prompt("Titulo", "Ingrese su estado civil").capitalize()
+        
+        while (estado_civil == None or not estado_civil.isalpha()) or estado_civil != "Soltero" and estado_civil != "Soltera" and estado_civil != "Casado" and estado_civil != "Casada" and estado_civil != "Divorciado" and estado_civil != "Divorciada" and estado_civil != "Viudo" and estado_civil != "Viuda" :
+            estado_civil = prompt("Titulo", "Ingrese su estado civil [Soltero/a, Divorciado/a, Casado/a, Viudo/a]")
+        
+        if estado_civil == "Soltero" or estado_civil == "Soltera":
+            estado_civil = "Soltero/a"
+        elif estado_civil == "Casado" or estado_civil == "Casada":
+            estado_civil = "Casado/a"
+        elif estado_civil == "Divorciado" or estado_civil == "Divorciada":
+            estado_civil = "Divorciado/a"
+        else:
+            estado_civil = "Viudo/a"
+
+        legajo = prompt(title="Legajo",prompt= "Ingrese su legajo")
+        
+        while legajo == None or not legajo.isdigit() or int(legajo) < 1000 or int(legajo) > 9999:
+            legajo = prompt(title="Legajo",prompt= "Ingrese un/a legajo valido")
+        legajo = int(legajo)
+
+        self.txt_apellido.delete(0, 100)
+        self.txt_apellido.insert(0, apellido)
+        self.txt_edad.delete(0, 100)
+        self.txt_edad.insert(0, edad)
+        self.combobox_tipo.set(estado_civil)
+        self.txt_legajo.delete(0, 100)
+        self.txt_legajo.insert(0, legajo)
 
 
 if __name__ == "__main__":
